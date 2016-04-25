dcf87f27f8177f79962cc73a7a5c8e4d46eebfec
olehalvor <olli408@hotmail.com>
2016-04-25 12:57:58 +0200

    sets spawnChance=0 if neighbour exists

--- main.py
+++ main.py
@@ -2,17 +2,19 @@
 import trafficlight as TL
 import utility as UTIL
 
+# -- Default values --
+overlapTime = 2
+minimumTimeSteps = 20
+spawnChanceLeft = 0.3
+spawnChanceRight = 0.5
+
 def createTrafficLights(n):
 	lightList = []
 	for i in range(n):
 		lightList.append(TL.trafficLight(UTIL.test(),2,20,0.3,0.5))
 		if (i>0):
 			lightList[i].leftNeighbour = lightList[i-1]
+			lightList[i].spawnChanceLeft = 0
 			lightList[i-1].rightNeighbour = lightList[i]
+			lightList[i-1].spawnChanceRight = 0
 	return lightList
-
-
-
-Lights = createTrafficLights(3)
-for l in lights:
-	print l
