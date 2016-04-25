a0128da770bc9b1d49829659aa5e8b6f4b4c0d57
olehalvor <olli408@hotmail.com>
2016-04-25 12:30:57 +0200

    first files

--- trafficlight.py
+++ trafficlight.py
@@ -1,13 +1,13 @@
 class trafficLight:
-	isGreen = False
-	CarCounterA = 0
-	carCounterB = 0
-	SpawnChanceA = 0.0
-	SpawnChanceB = 0.0
-	overlapTime = 2
-	pedestrianCounter = int
-	minimumTimeSteps = int
-	greenCounter = int #How many timesteps has passed while light has been green.
-	leftNeighbour = traffic_light object
-	rightNeighbours = traffic_light object
-	utility_function = function()
+	isGreen           = False
+	CarCounterLeft    = 0
+	carCounterRight   = 0
+	SpawnChanceLeft   = 0.0
+	SpawnChanceRight  = 0.0
+	overlapTime       = 0
+	pedestrianCounter = 0
+	minimumTimeSteps  = 0
+	stepCounter       = 0
+	leftNeighbour     = None
+	rightNeighbours   = None
+	utility_function  = None #Function?
