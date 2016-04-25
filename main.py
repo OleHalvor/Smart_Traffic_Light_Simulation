c3f3da4b17add4cf4fbdee0d27807f2d8dcefd59
olehalvor <olli408@hotmail.com>
2016-04-25 12:59:01 +0200

    m

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ overlapTime = 2
 minimumTimeSteps = 20
 spawnChanceLeft = 0.3
 spawnChanceRight = 0.5
+# -- Default values --
 
 def createTrafficLights(n):
 	lightList = []
 	for i in range(n):
-		lightList.append(TL.trafficLight(UTIL.test(),2,20,0.3,0.5))
+		lightList.append(TL.trafficLight(UTIL.test(),overlapTime,minimumTimeSteps,spawnChanceLeft,spawnChanceRight))
 		if (i>0):
 			lightList[i].leftNeighbour = lightList[i-1]
 			lightList[i].spawnChanceLeft = 0
 			lightList[i-1].rightNeighbour = lightList[i]
 			lightList[i-1].spawnChanceRight = 0
-	return lightList
+	return lightListts
